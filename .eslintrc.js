696fbe40a235cbc64a3912cc58fa39659a4faf99
Warren Sweetman <warren.sweetman@virginpulse.com>
2019-09-25 16:09:01 +1000

    reactful init

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/prop-types': ['off'],
+    'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-console': ['warn', { allow: ['info', 'error'] }],
+    curly: 'error',
+    'no-else-return': 'error',
+    'no-unneeded-ternary': 'error',
+    'no-useless-return': 'error',
+    'no-var': 'error',
+    'one-var': ['error', 'never'],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    strict: 'error',
+    'symbol-description': 'error',
+    yoda: ['error', 'never', { exceptRange: true }],
+  },
+};

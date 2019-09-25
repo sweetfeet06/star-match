40f86282b8745ff29618f96cbae0891cc5d99871
Warren Sweetman <warren.sweetman@virginpulse.com>
2019-09-25 16:49:56 +1000

    add game implemenation

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
+import Game from './Game';
 
-export function App({ initialData }) {
-  const [count, setCount] = useState(0);
+// STAR MATCH - V9
+const StarMatch = () => {
+  const [gameId, setGameId] = useState(1);
+  return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>;
+}
+
+
+export function App() {
   return (
-    <div>
-      <h1>{initialData.appName}</h1>
-      This is a sample stateful and server-side rendered React application.
-      <br />
-      <br />
-      Here is a button that will track how many times you click it:
-      <br />
-      <br />
-      <button onClick={() => setCount(count + 1)}>{count}</button>
-    </div>
+    <StarMatch />
   );
 }
---
+++ src/components/PlayNumber.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+const PlayNumber = (props) => (
+  <button
+    className="number"
+    style={{backgroundColor: colors[props.status]}}
+    onClick={() => props.onClick(props.number, props.status)}
+  >
+    {props.number}
+  </button>
+);
+
+export default PlayNumber;
